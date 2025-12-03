0c8b45cd54552d5e193e5f5982bd70b7554d05a2
z525098-ValentinDavid-Stolz <z525098@shibaura-it.ac.jp>
2025-12-03 05:17:12 +0100

    Update hw9-1.c

--- hw9-1.c
+++ hw9-1.c
@@ -1,1 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int a[2][2];
+    int i, j;
+
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < 2; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+
+    printf("You entered\n");
+
+    for(i = 0; i < 2; i++){
+        for(j = 0; j < 2; j++){
+            printf("%d ", a[i][j]);
+        }
+        printf("\n");
+    }
+
+    double determinat = a[0][0] * a[1][1] - a[0][1] * a[1][0];
+
+    printf("Determinant is %lf", determinat);
+
+    return 0;
+}
 

a95b15bc4ff317d1abb10442020667bea7bd0150
z525098-ValentinDavid-Stolz <z525098@shibaura-it.ac.jp>
2025-12-03 07:53:15 +0100

    Update ahw9-2.c

--- ahw9-2.c
+++ ahw9-2.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+
+#define SIZE 5
+
+int main(){
+
+    int nums[SIZE];
+    int i;
+
+    for(i = 0; i < SIZE; i++){
+        scanf("%d", &nums[i]);
+    }
+
+    //bubble sort
+
+    for(i = 0; i < SIZE - 1; i++){
+        for(int j = 0; j < SIZE - 1; j++){
+            if(nums[j] > nums[j+1]){
+                int temp = nums[j+1];
+                nums[j+1] = nums[j];
+                nums[j] = temp;
+            }
+        }
+    }
+
+    for(i = 0; i < SIZE; i++){
+        printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
 

3c892df9d96f4f012e71fd8c615163d55cb25393
z525098-ValentinDavid-Stolz <z525098@shibaura-it.ac.jp>
2025-12-03 06:48:33 +0100

    Update hw9-4.c

--- hw9-4.c
+++ hw9-4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    char hex[16] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+    int rgb[3];
+    int i;
+
+    for(i = 0; i < 3; i++){
+        scanf("%d", &rgb[i]);
+    }
+
+    for(i = 0; i < 3; i++){
+        int fc = rgb[i] / 16;
+        int sc = (((double)rgb[i] / (double)16) - fc) * 16;
+        printf("%c", hex[fc]);
+        printf("%c", hex[sc]);
+    }
+
+
+    return 0;
+}
+
 
